53875a0dba464fecc96388ca1f03cb8c7eb9fd9d
raghav287 <raghav@makes360.com>
2026-05-01 11:10:55 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,74 @@
+<!-- Footer -->
+<footer class="footer">
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="footer-column footer-about">
+                        <h3 class="footer-title">Makes360</h3>
+                        <p class="footer-about-text">
+                            Hi, I’m Samantha, wedding planner and designer, loving life in
+                            the wonderful New York. The nestam acuam nec odio the
+                            elementum.
+                        </p>
+                        <div class="footer-language">
+                            <i class="lni ti-world"></i>
+                            <select onchange="location = this.value">
+                                <option value="#0">English</option>
+                                <option value="#0">German</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 offset-md-1">
+                    <div class="footer-column footer-explore clearfix">
+                        <h3 class="footer-title">Explore</h3>
+                        <ul class="footer-explore-list list-unstyled">
+                            <li><a href="about.html">About</a></li>
+                            <li><a href="services.html">Services</a></li>
+                            <li><a href="portfolio.html">Portfolio</a></li>
+                            <li><a href="gallery.html">Gallery</a></li>
+                            <li><a href="blog.html">Blog</a></li>
+                            <li><a href="contact.html">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="footer-column footer-contact">
+                        <h3 class="footer-title">Get in touch</h3>
+                        <p class="footer-contact-text">
+                            Sector 74, Phase 8B, Mohali, PB - 160055
+                        </p>
+                        <div class="footer-contact-info">
+                            <p class="footer-contact-phone">
+                                <span class="ti-headphone-alt" tel="tel:919132103210"></span>+91 9132103210
+                            </p>
+                            <p class="footer-contact-mail">hr[@]makes360.com</p>
+                        </div>
+                        <div class="footer-about-social-list">
+                            <a href="#"><i class="ti-instagram"></i></a>
+                            <a href="#"><i class="ti-twitter"></i></a>
+                            <a href="#"><i class="ti-youtube"></i></a>
+                            <a href="#"><i class="ti-facebook"></i></a>
+                            <a href="#"><i class="ti-pinterest"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="footer-bottom-inner">
+                        <p class="footer-bottom-copy-right">
+                            © All Rights Reserved. Designed by
+                            <a href="https://1.envato.market/DuruThemes" target="_blank">Makes360</a>.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,123 @@
+<!-- Preloader -->
+<div class="preloader-bg"></div>
+<div id="preloader">
+    <div id="preloader-status">
+        <div class="preloader-position loader"><span></span></div>
+    </div>
+</div>
+<!-- Progress scroll totop -->
+<div class="progress-wrap cursor-pointer">
+    <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+        <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+    </svg>
+</div>
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg">
+    <div class="container">
+        <!-- Logo -->
+        <div class="logo-wrapper">
+            <a class="logo" href="index-2.html">
+                <img src="img/logo.png" class="logo-img" alt="" />
+            </a>
+            <!-- <a class="logo" href="index.html"> <h2>Florya</h2> </a> -->
+        </div>
+        <!-- Button -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar"
+            aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"><i class="ti-menu"></i></span>
+        </button>
+        <!-- Menu -->
+        <div class="collapse navbar-collapse" id="navbar">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        data-bs-auto-close="outside" aria-expanded="false">Home <i class="ti-angle-down"></i></a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="index-2.html" class="dropdown-item active"><span>Slider</span></a>
+                        </li>
+                        <li>
+                            <a href="index2.html" class="dropdown-item"><span>Video</span></a>
+                        </li>
+                        <li>
+                            <a href="index3.html" class="dropdown-item"><span>Parallax Image</span></a>
+                        </li>
+                        <li>
+                            <a href="index4.html" class="dropdown-item"><span>Slideshow</span></a>
+                        </li>
+                        <li>
+                            <a href="onepage.html" class="dropdown-item"><span>Onepage</span></a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about.html">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="services.html">Services</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        data-bs-auto-close="outside" aria-expanded="false">Portfolio <i class="ti-angle-down"></i></a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="portfolio-grid.html" class="dropdown-item"><span>Portfolio Grid</span></a>
+                        </li>
+                        <li>
+                            <a href="portfolio-masonry.html" class="dropdown-item"><span>Portfolio Masonry</span></a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        data-bs-auto-close="outside" aria-expanded="false">Blog <i class="ti-angle-down"></i></a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="blog-2-column.html" class="dropdown-item"><span>Blog 2 Column</span></a>
+                        </li>
+                        <li>
+                            <a href="blog-right-sidebar.html" class="dropdown-item"><span>Blog Right Sidebar</span></a>
+                        </li>
+                        <li>
+                            <a href="blog-left-sidebar.html" class="dropdown-item"><span>Blog Left Sidebar</span></a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        data-bs-auto-close="outside" aria-expanded="false">Pages <i class="ti-angle-down"></i></a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="services-page.html" class="dropdown-item"><span>Services Details</span></a>
+                        </li>
+                        <li>
+                            <a href="portfolio-page.html" class="dropdown-item"><span>Portfolio Page</span></a>
+                        </li>
+                        <li>
+                            <a href="post.html" class="dropdown-item"><span>Post Single</span></a>
+                        </li>
+                        <li class="dropdown-submenu dropdown">
+                            <a class="dropdown-item dropdown-toggle" data-bs-toggle="dropdown"
+                                data-bs-auto-close="outside" aria-expanded="false" href="#"><span>Other Pages <i
+                                        class="ti-angle-right"></i></span></a>
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <a href="coming-soon.html" class="dropdown-item"><span>Coming Soon</span></a>
+                                </li>
+                                <li>
+                                    <a href="404.html" class="dropdown-item"><span>404 Error</span></a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="gallery.html">Gallery</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.html">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
